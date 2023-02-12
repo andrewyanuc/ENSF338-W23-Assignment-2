ae6dc50383af70b5cbefd79621a327c71f9c91b1
Katherine Tubang <113137650+kdafrose@users.noreply.github.com>
2023-02-11 20:25:51 -0700

    Update ex2.2.py

--- ex2.2.py
+++ ex2.2.py
@@ -8,20 +8,6 @@ import timeit
 
 sys.setrecursionlimit(20000) 
 
-filename = "/Users/owner/Documents/YEAR2/W23_Y2/ENSF338/Assignment2/ex2.json"
-inserted_array =[]
-
-with open(filename, "r") as file:
-    data = json.load(file)
-    #data has 10 arrays 
-    i = 0 
-    for items in data:
-        j = 0
-        while j < len(data[i]):
-            inserted_array.append(data[i][j])
-            j = j +1
-        i = i + 1
-
 
 def func1(arr, low, high): 
     '''
@@ -32,9 +18,9 @@ def func1(arr, low, high):
     low (integer) = index in array, should be the smallest number in arr
     high (integer) = index in array, that should be the highest value in arr
     '''
-    if low < high:
-        pi = func2(arr, low, high) 
-        func1(arr, low, pi-1) 
+    if low < high: 
+        pi = func2(arr, low, high)  
+        func1(arr, low, pi-1)
         func1(arr, pi + 1, high)
 
 
@@ -47,31 +33,42 @@ def func2(array, start, end):
     end (integer) = index value in array, bigger than start
     '''
     p = array[start] 
-    low = start + 1 
+    low = start + 1     
     high = end 
-
+   
     while True: 
+      
 
-        while low <= high and array[high] >= p:
+        while low <= high and array[high] >= p: 
             high = high - 1 
- 
+
 
         while low <= high and array[low] <= p: 
-            low = low + 1
-          
+            low = low + 1 
 
         if low <= high:
             array[low], array[high] = array[high], array[low]
+            
         else:
             break
 
-    array[start], array[high] = array[high], array[start]
+    array[start], array[high] = array[high], array[start] 
 
     return high
 
 
-executed_time = timeit.timeit(lambda:func1(inserted_array, 0, (len(inserted_array) - 1)) , number =1)
-print(executed_time)
+filename = "/Users/owner/Documents/YEAR2/W23_Y2/ENSF338/Assignment2/ex2.json"
+times =[]
 
+with open(filename, "r") as file:
+    data = json.load(file)
+    #data has 10 arrays 
+    i = 0 
+    for items in data:
+        executed_time = timeit.timeit(lambda:func1(data[i], 0, (len(data[i]) - 1)) , number =1)
+        times.append(executed_time)
+        i = i + 1
+    
+print("Timing for each array in ex2.json: ", times)
 file.close()
 

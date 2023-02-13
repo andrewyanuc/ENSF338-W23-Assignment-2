c5841b2d20b815cca3b79a2e8b1bffb6d19d29cb
Katherine Tubang <113137650+kdafrose@users.noreply.github.com>
2023-02-12 23:20:54 -0700

    Update ex2.4.py

--- ex2.4.py
+++ ex2.4.py
@@ -3,10 +3,11 @@ This is exercise 2 for Assignment 2 ENSF 338
 '''
 
 import json
+import random
 import sys
 import timeit
 
-sys.setrecursionlimit(20000) # set the maximum recursion depth to 20000, as default is 1000
+sys.setrecursionlimit(20000) 
 
 def func1(arr, low, high): 
     '''
@@ -18,9 +19,8 @@ def func1(arr, low, high):
     high (integer) = index in array, that should be the highest value in arr
     '''
     if low < high: 
-        # 1 OPERATION
-        pi = func2(arr, low, high) # calls for the second function 
-        func1(arr, low, pi-1) # pi is the high position, and minus one as current position is already sorted
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
         func1(arr, pi + 1, high)
 
 
@@ -34,19 +34,16 @@ def func2(array, start, end):
     '''
     # We are going to optimize this function choosing a better pivot value rather than the start of the array block
     # if there are items in this this block of array with more than 2 elements
-    # mid = start + ( (end - start) // 2)
-    # make a new array with only the first, last and the middle element 
-    # sort those elements in that array from smallest to largest
-    # as there are only 3 elements, the median value between them would be the second item
+    # choose a random number, inbetween start and end to avoid reusing same pivot
+    # switch out chosen pivot to start index
     # else:
         # there only 2 elements between them 
         # let p equal the first value in that array
 
     if (end - start >= 2):
-        mid = start + ((end -start) // 2)
-        pivots = [array[start], array[mid],array[end]]
-        pivots.sort()
-        p = pivots[1]
+        pivot_index = random.randint(start, end)
+        array[start], array[pivot_index]= array[pivot_index], array[start]
+        p = array[start]
 
     else:
         p = array[start] 
@@ -54,6 +51,7 @@ def func2(array, start, end):
     low = start + 1
     
     high = end 
+
    
     while True: 
 
@@ -75,18 +73,36 @@ def func2(array, start, end):
     return high
 
 
-filename = "/Users/owner/Documents/YEAR2/W23_Y2/ENSF338/Assignment2/ex2.json"
+filename_r = "/Users/owner/Documents/YEAR2/W23_Y2/ENSF338/Assignment2/ex2.json"
+filename_w = "/Users/owner/Documents/YEAR2/W23_Y2/ENSF338/Assignment2/ex2.5.json"
 times =[]
+times_op =[]
 
-with open(filename, "r") as file:
+with open(filename_r, "r") as file:
     data = json.load(file)
+
     #data has 10 arrays 
     i = 0 
     for items in data:
         executed_time = timeit.timeit(lambda:func1(data[i], 0, (len(data[i]) - 1)) , number =1)
         times.append(executed_time)
         i = i + 1
-    
-print("Timing for each array in ex2.json: ", times)
-file.close()
 
+with open(filename_w, "w") as write:
+    json.dump(data,write)
+write.close()
+
+with open(filename_w, "r") as file_op:
+    alter = json.load(file_op)
+
+    i = 0 
+    for items in data:
+        executed_time = timeit.timeit(lambda:func1(alter[i], 0, (len(alter[i]) - 1)) , number =1)
+        times_op.append(executed_time)
+        i = i + 1
+
+print("Timing for each array in ex2.json: ", times)
+print("Timing for each altered arrays: ", times_op)
+
+file.close()
+file_op.close()
